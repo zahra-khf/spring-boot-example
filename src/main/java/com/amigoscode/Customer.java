7c39c50ebeabc709a2beff26ee89d16193933330
zkhalifa <zkhalifa@et.esiea.fr>
2022-12-20 22:23:52 +0100

    first commit

---
+++ src/main/java/com/amigoscode/Customer.java
@@ -0,0 +1,90 @@
+package com.amigoscode;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+    private int id , age;
+    private String name , email ;
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public Customer() {
+    }
+
+    public Customer(int id,
+                    int age,
+                    String name,
+                    String email) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && age == customer.age && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, age, name, email);
+    }
+}
+
+
---
+++ src/main/java/com/amigoscode/Main.java
@@ -0,0 +1,14 @@
+package com.amigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//to say that it's a spring application
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+
+    }
+
+}
